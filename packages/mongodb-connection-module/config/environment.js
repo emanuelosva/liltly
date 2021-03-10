6802ce554ec9755877eba4d21833433303ae9762
emanuelosva <emanuelosva@gmail.com>
2021-03-09 19:02:37 -0600

    chore(Fix): fix path to .env file

--- packages/mongodb-connection-module/config/environment.js
+++ packages/mongodb-connection-module/config/environment.js
@@ -2,18 +2,19 @@ const dotenv = require('dotenv')
 const path = require('path')
 const { supportedEnvs } = require('../src/utils/constants')
 
-
 let environment
-let env = '.env'
-const pathToEnvFile = path.resolve(__dirname, '../', env)
+const env = '.env'
+const pathToEnvFile = path.resolve(__dirname, '../../..', env)
 
-switch(process.env.NODE_ENV) {
+switch (process.env.NODE_ENV) {
   case supportedEnvs.PRODUCTION:
     environment = ''
     break
   case supportedEnvs.DEVELOPMENT:
     environment = 'DEV_'
     break
+  default:
+    environment = 'DEV_'
 }
 
 dotenv.config({ path: pathToEnvFile })
